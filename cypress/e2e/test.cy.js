699a29f87fac0660d9540bf15cc90c4e31a17511
Artturi Kerminen <kerminenartturi@gmail.com>
2025-05-05 13:09:36 +0300

    Add sample cypress tests

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Main page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
+
+  it('has title', () => {
+    cy.get('[data-testid="cypress-heading"]').should("exist")
+      .should('have.text', 'Cypress')
+  })
+
+  it('has button', () => {
+    cy.get('[data-testid="cypress-button"]').should("exist")
+      .should('have.text', 'count is 0')
+  })
+})
+
+describe('Button', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/')
+  })
+
+  it('increases count when clicked', () => {
+    cy.get('[data-testid="cypress-button"]').should("exist")
+      .should('have.text', 'count is 0')
+
+    cy.get('[data-testid="cypress-button"]').click()
+
+    cy.get('[data-testid="cypress-button"]').should("exist")
+      .should('have.text', 'count is 1')
   })
 })
